ea50e0c9058b96ed8af10b1b50b7c56f38f2c1d2
MonicaGodwin <monicagodwinn@gmail.com>
2026-09-15 10:59:38 +0100

    my commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
+import time
 import sqlite3
+
 connection = sqlite3.connect("tasks.db")
 cursor = connection.cursor()
 
@@ -8,8 +9,8 @@ cursor.execute("""
         id INTEGER PRIMARY KEY,
         description TEXT,
         status TEXT,
-        createdAt TEXT, 
-        updatedAt TEXT            
+        created_at TEXT, 
+        updated_at TEXT            
         )
 """)
 
@@ -21,13 +22,61 @@ while True:
         f"3. Delete tasks\n"
         f"4. Mark as in-progress or done\n"
         f"5. List all tasks\n"
-        f"6. List by status\n"
-        f"7. Exit\n"
+        f"6. List done\n"
+        f"7. List not done\n"
+        f"8. List in-progress\n"
+        f"9. Exit\n"
         f"\n choose an option(1, 2, 3, 4, 5, 6, 7)\n"
     )
 
     if user_input == "1":
-        cursor.
+        description = input("Enter task description:\n").strip()
+        if not description.isalpha():
+            print("invalid input\n")
+            continue
+        status = "In-progress"
+        created_at = time.ctime()
+        updated_at = time.ctime()
+        insert = cursor.execute(
+            "INSERT INTO tasks(description, status, created_at) VALUES(?,?,?,?)",
+            (description, status, created_at, updated_at)
+        )
+        if insert.rowcount > 0:
+            print("task added successfully")
+        else:
+            print("could not add task")
+        connection.commit()
+
+    if user_input == "2":
+        task_id = int(input("Enter an id to update\n"))
+        new_description = input("Enter new description\n")
+        status = "In-progress"
+        if not new_description.isalpha():
+            print("Invalid input\n")
+            continue
+        cursor.execute(
+            "UPDATE tasks SET description = ? WHERE id = ?",
+            (new_description, task_id,)
+        )
+        if cursor.rowcount > 0:
+            print("Task updated")
+        else:
+            print("Task not found")
+        connection.commit()
+
+    if user_input == "3":
+        delete_id = int(input("Enter an id to delete\n"))
+        cursor.execute(
+            "DELETE FROM tasks WHERE id = ?",
+            (delete_id,)
+        )
+        if cursor.rowcount > 0:
+            print("Task deleted")
+        else:
+            print("Task not found")
+        connection.commit()
+
+
 
 
     # if user_input == "1":
