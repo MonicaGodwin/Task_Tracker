ccd2d98a81193866fd8c9200ca22aff923e0a304
MonicaGodwin <monicagodwinn@gmail.com>
2026-09-16 10:56:43 +0100

    my commit

--- main.py
+++ main.py
@@ -21,26 +21,27 @@ else:
         tasks = json.load(data)
        
 if len(sys.argv) < 2:
-    print("Usage: python main.py [add | delete | list]")
+    print("Usage: python main.py [add | list | delete]")
     sys.exit()
     
 command = sys.argv[1].lower()
 
         
 if command == "add":
-    if len(sys.argv) < 3:
+    if len(sys.argv) > 3:
+        print('Usage: python3 [main.py] [add] ["Task description"]')
+        sys.exit()
+    elif len(sys.argv) < 3:
         print("Error: Please provide a task description.")
-        print('Usage: python3 main.py add "Task description"')
         sys.exit()
         
     description = sys.argv[2]
-    description = description.strip()
     next_id = len(tasks) + 1 # Calculates the next task id position 
     current_time = time.ctime()
     new_tasks = {
     "id": next_id,
     "description": description,
-    "status": "Todo",  # Tasks start as "todo" by default
+    "status": "todo",  # Tasks start as "todo" by default
     "created_at": current_time,
     "updated_at": current_time
     }
@@ -52,6 +53,13 @@ if command == "add":
 
 
 elif command == "list":
+    if len(sys.argv) > 2:
+        print(f"Usage: python3 [main.py] [list]")
+        sys.exit()
+    elif len(sys.argv) < 2:
+        print(f"Error: please provide the command argument\n")
+        sys.exit()
+        
     if len(tasks) == 0:
         print("No task to display.")
     else:
@@ -59,11 +67,11 @@ elif command == "list":
             print(f"{task['id']} - {task['description']} - {task['status']} - {task['created_at']} - {task['updated_at']}")
 
 elif command == "delete":
-    if len(sys.argv) < 3:
-        print(
-            f"Error: please provide a task id number to delete\n"
-            f'Usage: python3 main.py delete "The task Id to delete"'
-        )
+    if len(sys.argv) > 3:
+        print(f'Usage: python3 [main.py] [delete] [The task Id to delete]')
+        sys.exit()
+    elif len(sys.argv) < 3:
+        print(f"Error: please provide a task id number to delete\n")
         sys.exit()
 
     task_id = sys.argv[2]
@@ -88,14 +96,14 @@ elif command == "delete":
     else:
         print("Task Id not found")
     
-    
 elif command == "update":
-    if len(sys.argv) < 4:
-        print(
-            f"Error: please provide a task id number to update\n"
-            f'Usage: python3 main.py update "The task Id to update" New description'
-        )
+    if len(sys.argv) > 4:
+        print('Usage: python3 [main.py] [update] [The task Id to update] ["New description"]')
         sys.exit()
+    elif len(sys.argv) < 4:
+        print("Error: please provide a task id or description to update\n")
+        sys.exit()
+
     updated = False
     task_id = sys.argv[2]
     description = sys.argv[3]
@@ -120,14 +128,14 @@ elif command == "update":
 
 elif command == "mark-done" or command == "mark-in-progress":
     if len(sys.argv) < 3:
-        print(
-            f"Error: please provide a task id number to mark done\n"
-            f'Usage: python3 main.py mark-done "The task Id to mark done"'
-        )
+        print('Usage: python3 [main.py] [mark-done] | [mark-in-progress] ["The task Id to mark done"]')
+        sys.exit()
+    elif len(sys.argv) > 3:
+        print("Error: please provide a task id number to mark done")
         sys.exit()
 
     status_update = False
-    # status = sys.argv[1]
+    status = ""
     current_time = time.ctime
     if command == "mark-done":
         status = "done"
@@ -153,32 +161,23 @@ elif command == "mark-done" or command == "mark-in-progress":
     else:
         print(f"Could not mark status as {status}")
 
-elif command == "list":
-
+elif command == "list-by-status":
     if len(sys.argv) < 3:
-
-        print(
-            f'Usage: python3 main.py list [todo] | [in-progress] | [done] | [not-done]'
-        )
-
+        print('Usage: python3 main.py list [todo] | [in-progress] | [done] | [not-done]')
+        sys.exit()
+    elif len(sys.argv) > 3:
+        print("Error: Argument should not be more than 3")
         sys.exit()
 
     list_task = False
-    task_status = sys.argv[2]
-
+    task_status = sys.argv[2].lower()
     for specified_task in tasks:
-
-        if task_status == "not-done":
-
-            if specified_task["status"] in ["Todo", "In-progress"]:
+        if task_status == "done":
+            if specified_task["status"] in ["todo", "in-progress"]:
                 list_task = True
                 print(specified_task)
-
         elif specified_task["status"] == task_status:
-
             list_task = True
             print(specified_task)
-
     if not list_task:
-
         print(f"No task found with the status: {task_status}")
